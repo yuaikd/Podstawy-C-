e831e4019ffc2f1a67c44d632d90a8d5a2d32840
yuaikd <140353790+yuaikd@users.noreply.github.com>
2025-06-20 10:35:40 +0000

    Add files via upload

---
+++ Zadanie 1_rekurencje/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1_rekurencje
+{//. Napisz funkcję , która oblicza wartość silni z zadanej przez argument wywołania funkcji
+   // wartości liczby naturalnej.Utwórz wersję rekurencyjną tej funkcji.
+   // Napisz program testowy wykorzystujący napisaną funkcję
+  class Program
+    {
+        static long silnia(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            else
+                return n * silnia(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę naturalną do obliczenia silni:");
+            int n = int.Parse(Console.ReadLine());
+            long wynik = silnia(n);
+            Console.WriteLine($"Silnia z {n} wynosi: {wynik}");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Zadanie 2_rekurencje/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_rekurencje
+{//Napisz funkcję, która wyznacza n-ty wyraz ciągu Fibonacciego zadany przez argument
+   // wywołania funkcji wartości liczby naturalnej.Utwórz wersję rekurencyjną tej funkcji.
+   // Napisz program testowy wykorzystujący napisaną funkcję.
+
+    class Program
+    {
+        static long fib(long n)
+        {if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę naturalną do obliczenia n-tego wyrazu ciągu Fibonacciego:");
+            long n = long.Parse(Console.ReadLine());
+            if (n < 0)
+             Console.WriteLine("Liczba musi być liczbą naturalną.");
+            else Console.WriteLine("fib({0})={1}", n, fib(n));
+            Console.ReadKey();
+
+        }
+    }
+}
